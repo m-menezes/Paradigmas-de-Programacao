0ba3ea9a9cfe1751e0e8710da10e43ad00d258c0
m3nezes <m3nezes@gmail.com>
2017-03-14 17:52:25 -0300

    Update TutorialImport.py

--- TutorialImport.py
+++ TutorialImport.py
@@ -2,7 +2,7 @@
 #Podemos utilizar o modulo ja existente "os" que permite interagir com o sistema operacional
 import os
 #entao utilizamos uma das funcoes que e, chdir que permite trocar o diretorio de trabalho
-os.chdir('c:/Users/Marcelo/Documents')
+os.chdir('c:/Users/Marcelo/Desktop')
 #apos trocar o diretorio conseguimos importar o arquivo com nossas funcoes
 import funcoes
 #e utiliza-las
