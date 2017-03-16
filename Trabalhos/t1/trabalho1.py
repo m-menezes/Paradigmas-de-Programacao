9243bfd3dc67518ffdef33e4ad3be22df27ab070
Marcelo Menezes <m3nezes@gmail.com>
2017-03-16 10:22:18 -0300

    Update trabalho1.py

--- Trabalhos/t1/trabalho1.py
+++ Trabalhos/t1/trabalho1.py
@@ -1,45 +1,46 @@
-#ex:1
-def somaQuad(x,y):
-    return ((x*x)+(y*y))
+#Parte 1
+    #ex:1
+    def somaQuad(x,y):
+        return ((x*x)+(y*y))
 
-#ex:2
-def hasEqHeads(l1,l2):
-    return (l1[0] == l2[0])
+    #ex:2
+    def hasEqHeads(l1,l2):
+        return (l1[0] == l2[0])
 
-#ex:3
-def adicionaSr(lis):
-    return ('Sr. ' + lis)
-def ex3(lis):
-    return list(map(adicionaSr, lis))
+    #ex:3
+    def adicionaSr(lis):
+        return ('Sr. ' + lis)
+    def ex3(lis):
+        return list(map(adicionaSr, lis))
 
-#ex:4
-def espaco(string):
-    return(' ' == string)
-def ex4(string):
-    return len(filter(espaco,string))
+    #ex:4
+    def espaco(string):
+        return(' ' == string)
+    def ex4(string):
+        return len(filter(espaco,string))
 
-#ex:5
-def calculo5(n):
-    return((((3*n)*2) + 2)/(n + 1))
-def ex5(n):
-    return map(calculo5,n)
+    #ex:5
+    def calculo5(n):
+        return((((3*n)*2) + 2)/(n + 1))
+    def ex5(n):
+        return map(calculo5,n)
 
-#ex:6
-def neg(nlis):
-    return(nlis<0)
-def ex6(n):
-    return filter(neg, n)
+    #ex:6
+    def neg(nlis):
+        return(nlis<0)
+    def ex6(n):
+        return filter(neg, n)
 
-#ex:7
-def intervalo(nlis):
-    return (nlis>=10) and (nlis<=100)
-def ex7(n):
-    return filter(intervalo,n)
+    #ex:7
+    def intervalo(nlis):
+        return (nlis>=10) and (nlis<=100)
+    def ex7(n):
+        return filter(intervalo,n)
 
-#ex:8
-def somentepares(nlis):
-    return (nlis%2 == 0)
-def ex8(n):
-    return filter(somentepares,n)
+    #ex:8
+    def somentepares(nlis):
+        return (nlis%2 == 0)
+    def ex8(n):
+        return filter(somentepares,n)
 
-#ex:9
+    #ex:9
