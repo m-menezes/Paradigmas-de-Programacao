a89602e23b87a9fe66a13d0dddb1fac118c42457
m3nezes <m3nezes@gmail.com>
2017-03-23 17:10:13 -0300

    ..

--- Tutorial lxml.py
+++ Tutorial lxml.py
@@ -5,4 +5,4 @@ Execute o comando
     pip install requests no Prompt
 se o comando não executar baixe o arquivo lxml****.exe (32 ou 64 dependendo do sistema)
     https://pypi.python.org/pypi/lxml/3.4.1
-para a versão do python instalada
+para a versão do python instalada.
