268bb134e03de57eaeacfbfe3bb6667a0235d62c
m3nezes <m3nezes@gmail.com>
2017-03-23 17:04:12 -0300

    Tutorial lxml

---
+++ Tutorial lxml.py
@@ -0,0 +1,8 @@
+Error:
+    from lxml import html, etree
+    ImportError: No module html, etree
+Execute o comando
+    pip install requests no Prompt
+se o comando não executar baixe o arquivo lxml****.exe (32 ou 64 dependendo do sistema)
+    https://pypi.python.org/pypi/lxml/3.4.1
+para a versão do python instalada
