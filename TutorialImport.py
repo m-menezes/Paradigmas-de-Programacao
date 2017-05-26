5afe51fee70ac8d0e5c0651e74a091a593816d77
Marcelo <m3nezes@gmail.com>
2017-05-26 20:00:51 -0300

    Correção

--- TutorialImport.py
+++
@@ -1,9 +0,0 @@
-#Tutorial Importar Modulos/Arquivos no Python Windows
-#Podemos utilizar o modulo ja existente "os" que permite interagir com o sistema operacional
-import os
-#entao utilizamos uma das funcoes que e, chdir que permite trocar o diretorio de trabalho
-os.chdir('c:/Users/Marcelo/Desktop')
-#apos trocar o diretorio conseguimos importar o arquivo com nossas funcoes
-import funcoes
-#e utiliza-las
-funcoes.soma25(5)
